2cf3cff4fdd6760b7190e0aab075ad768c5d07dc
Nuutti Varvikko <nvarvikko@gmail.com>
2021-09-20 02:39:22 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Font installer
+
+A simple script to quickly install font files from a zip file.
+
---
+++ install_font.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+DEST=~/.local/share/fonts
+SRC=$1
+
+mkdir -p $DEST
+
+if [ -z $SRC ]; then
+	echo "Please specify file."
+	exit 1
+fi
+
+if [ ! -e $SRC ]; then
+	echo "FIle $SRC does not exist."
+	exit 1
+fi
+
+if [[ ! ($SRC == *.zip) ]]; then
+	echo "File must be zip."
+	exit 1
+fi
+
+LEN=${#SRC}
+DIR=$(echo $SRC | cut -c 1-$((LEN - 4)) | tr '/' '_')
+unzip -qq $SRC -d $DIR
+
+FONTS=$(find $DIR -name '*.ttf')
+for FONT in $FONTS
+do
+	mv $FONT $DEST
+done
+
+rm -rf $DIR
+
